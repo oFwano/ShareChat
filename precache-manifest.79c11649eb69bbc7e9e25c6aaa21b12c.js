498e0e901ae71cab54b4f511634f48038516d79e
Francis Wan <francisw@sfu.ca>
2020-10-11 19:51:14 -0700

    Updates

---
+++ precache-manifest.79c11649eb69bbc7e9e25c6aaa21b12c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d30d73da20aed4d972d9ab6d48c8d0e",
+    "url": "/sharechat/index.html"
+  },
+  {
+    "revision": "88181a86d897def80324",
+    "url": "/sharechat/static/css/main.3317097f.chunk.css"
+  },
+  {
+    "revision": "d13028847f31d3adc298",
+    "url": "/sharechat/static/js/2.221d4a5d.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/sharechat/static/js/2.221d4a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88181a86d897def80324",
+    "url": "/sharechat/static/js/main.37be0944.chunk.js"
+  },
+  {
+    "revision": "08ca76ba9daa5f5275c8",
+    "url": "/sharechat/static/js/runtime-main.00abdce6.js"
+  }
+]);
